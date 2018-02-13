31bfababba1c47e52efd364da9e18d180a770443
sergey-shevchenko <krieghund.shevchenko@gmail.com>
2018-02-13 20:17:44 +0100

    added redis db config

--- config/database.php
+++ config/database.php
@@ -78,6 +78,19 @@ return [
             'prefix' => '',
         ],
 
+        'redis' => [
+
+            'client' => 'predis',
+
+            'default' => [
+                'host' => env('REDIS_HOST', 'localhost'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
+
+        ],
+
     ],
 
     /*
